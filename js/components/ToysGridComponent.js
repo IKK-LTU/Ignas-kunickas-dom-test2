42e130e35e7dd388c8c0a67723acf95ed91eb162
IKK-LTU <ignas.kunickas@gmail.com>
2021-11-15 16:41:44 +0200

    6:  initFetch function was  created to download data from the server.  7: 1. Loading animation added.

--- js/components/ToysGridComponent.js
+++ js/components/ToysGridComponent.js
@@ -1,13 +1,54 @@
-class ToyGridComponent{
-    constructor() {
-        this.state = {
-            toys:[]
+class ToyGridComponent {
+  constructor() {
+    this.state = {
+      loading: false,
+      toys: [],
+    };
+    this.init();
+  }
+
+  initFetch = () => {
+    setTimeout(() => {
+      API.fetchToys(
+        (toys) => {
+        this.state.loading = false;
+              this.saveToys(toys);
+              console.log('parsiusta')
+        },
+        (err) => {
+          alert(err);
+          this.state.loading = false;
+          this.render();
         }
-        this.init();
-    }
+      );
+    }, 1000);
+  };
+  saveToys = (toys) => {
+      this.state.toys = toys;
+      console.log(toys);
+      
+    this.render();
+  };
+
     init = () => {
-            this.htmlElement = document.createElement("div");
-            this.htmlElement.className = "row g-3";
-        this.htmlElement.innerHTML = `<div><img src="../../assets/loading.gif"/></div>`;
-    }
+      this.state.loading = true;
+    this.htmlElement = document.createElement("div");
+      this.htmlElement.className = "row g-3";
+      this.initFetch();
+    this.render();
+  };
+
+  render = () => {
+    const { loading, toys } = this.state;
+    if (loading) {
+      this.htmlElement.innerHTML = `<div><img src="../../assets/loading.gif"/></div>`;
+    } else if(toys.length>0) {
+      this.htmlElement.innerHTML = `<div><img src="../../assets/loading.gif"/></div>`;
+
+      }
+    else {
+            this.htmlElement.innerHTML = `<h1>Nera itemu</h1>`;
+        
+      }
+  };
 }
